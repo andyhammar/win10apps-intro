add78b9fc7f29f93f69849222ae88391e038f9a2
Andreas Hammar <andreas.hammar@jayway.com>
2015-10-21 12:34:54 +0200

    demo code complete

---
+++ src/UwpIntro/UwpIntro/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using UwpIntro.ViewModels;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UwpIntro
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private MainPageVm _vm;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            //textBox.Text = "hej!";
+            DataContext = _vm = new MainPageVm();
+        }
+
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            await _vm.Init();
+        }
+
+        private void listView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            Frame.Navigate(typeof (ItemPage), e.ClickedItem);
+        }
+    }
+}
---
+++ src/UwpIntro/UwpIntro/ViewModels/MainPageVm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Web.Syndication;
+using UwpIntro.Annotations;
+
+namespace UwpIntro.ViewModels
+{
+    public class NewsItemVmi
+    {
+        public string Title { get; set; }
+        public string Published { get; set; }
+        public string Image { get; set; }
+    }
+
+    public class MainPageVm : INotifyPropertyChanged
+    {
+        private TimeSpan _time;
+
+        public MainPageVm()
+        {
+            NewsItems = new ObservableCollection<NewsItemVmi>();
+
+            Time = DateTime.Now.TimeOfDay;
+
+            if (DesignMode.DesignModeEnabled)
+            {
+                NewsItems.Add(new NewsItemVmi { Title = "item 1", Published = "2015-01-01", Image = "/Assets/StoreLogo.png" });
+                NewsItems.Add(new NewsItemVmi { Title = "item 2", Published = "2015-01-01", Image = "/Assets/StoreLogo.png" });
+                NewsItems.Add(new NewsItemVmi
+                {
+                    Title = "item 3 with longer name",
+                    Published = "2015-01-01",
+                    Image = "/Assets/StoreLogo.png"
+                });
+                NewsItems.Add(new NewsItemVmi
+                {
+                    Title = "item 4 (design time only)",
+                    Published = "2015-01-01",
+                    Image = "/Assets/StoreLogo.png"
+                });
+
+            }
+        }
+
+        public async Task Init()
+        {
+            var client = new SyndicationClient();
+            var feed = await client.RetrieveFeedAsync(
+                new Uri("http://rss.cnn.com/rss/edition_technology.rss", UriKind.Absolute));
+
+            foreach (var item in feed.Items)
+            {
+                var element = item.
+                    ElementExtensions.FirstOrDefault(e => e.NodeName == "thumbnail");
+                var attribute = element.AttributeExtensions.FirstOrDefault(ae => ae.Name == "url");
+                var uri = attribute?.Value;
+                var imageUri = uri ?? "https://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED.png";
+                NewsItems.Add(new NewsItemVmi
+                {
+                    Title = item.Title.Text,
+                    Image = imageUri,
+                    Published = item.PublishedDate.ToString("g")
+                });
+            }
+        }
+        public ObservableCollection<NewsItemVmi> NewsItems { get; set; }
+
+        public TimeSpan Time
+        {
+            get { return _time; }
+            set { _time = value; OnPropertyChanged();}
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
